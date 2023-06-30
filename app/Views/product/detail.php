35219ca3560f36855c2f946de2a0a1e0e377b36d
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-01 02:42:14 +0700

    update style pemanggilan variable product

--- app/Views/product/detail.php
+++ app/Views/product/detail.php
@@ -9,12 +9,12 @@ Detail Product
 <div class="card my-4" style="max-width: 540px;">
     <div class="row g-0">
         <div class="col-md-4">
-            <img src="/img/<?php echo $product[0]['image']; ?>" class="img-fluid rounded-start" alt="...">
+            <img src="/img/<?php echo $product->image ?>" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title"><?php echo $product[0]['name']; ?></h5>
-                <p class="card-text"><small class="text-muted"><?php echo $product[0]['category_name']; ?></small></p>
+                <h5 class="card-title"><?php echo $product->name ?></h5>
+                <p class="card-text"><small class="text-muted"><?php echo $product->category_name ?></small></p>
             </div>
         </div>
     </div>
