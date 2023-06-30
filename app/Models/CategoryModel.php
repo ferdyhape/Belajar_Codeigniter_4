5c50584d06c7f79ec2396402bd17da5e5105198f
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-30 14:20:55 +0700

    basa basi dengan model

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -20,24 +20,6 @@ class CategoryModel extends Model
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 
     // RELATIONSHIP
     public function products()
