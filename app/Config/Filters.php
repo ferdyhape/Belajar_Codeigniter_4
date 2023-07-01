9b41d127fa7962672250067659f13516c0de1c44
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-01 20:30:14 +0700

    menambahkan filter login sebagai middleware

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,10 +2,11 @@
 
 namespace Config;
 
-use CodeIgniter\Config\BaseConfig;
+use App\Filters\LoginFilter;
 use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\Honeypot;
+use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
 
@@ -21,6 +22,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'auth'          => LoginFilter::class,
     ];
 
     /**
